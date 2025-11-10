0bc37337e1261b81f6e182285f75bec9efd78fa5
mason.neurauter@gmail.com <mason.neurauter@gmail.com>
2025-11-09 23:40:54 -0800

    added to main
    everything implemented except for the ai

--- Connect4/Connect4.cpp
+++ Connect4/Connect4.cpp
@@ -1,20 +1,125 @@
-// Connect4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+
+// tells compiler the functions exists
+void resetGrid();
+void printGrid();
+int dropCoin(int column);
+void opponentTurn();
+int takeColInput();
+void myTurn();
+
+// constants
+std::string EMPTY = "( )";
+std::string ME = "(M)";
+std::string OPPONENT = "(O)";
+
+std::vector<std::vector<std::string>> grid; // grid[level][column]
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    resetGrid();
+
+    std::cout << "Do I go first? (yes/no)\n";
+    std::string first;
+    std::cin >> first;
+    system("cls");
+
+    while (true) {
+        printGrid();
+        if (first != "yes") {
+            opponentTurn();
+            system("cls");
+            printGrid();
+            myTurn();
+        }
+        else {
+            myTurn();
+            system("cls");
+            printGrid();
+            opponentTurn();
+        }
+        system("cls");
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void myTurn() {
+    std::cout << "-----------------" << "\n";
+    std::cout << "Your Turn" << "\n";
+    int col = takeColInput(); // computer needs to do this
+    int lvl = dropCoin(col);
+    while (lvl == 0) {
+        std::cout << "Column is full, pick another" << "\n";
+        col = takeColInput();
+        lvl = dropCoin(col);
+    }
+    grid[lvl-1][col-1] = ME;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void resetGrid() 
+{
+    grid = {};
+    for (int i = 0; i < 6; i++) {
+        std::vector<std::string> temp = {};
+        for (int k = 0; k < 7; k++) {
+            temp.push_back(EMPTY);
+        }
+        grid.push_back(temp);
+    }
+}
+
+void printGrid() 
+{
+    for (int i = 0; i < 6; i++) {
+        for (int k = 0; k < 7; k++) std::cout << grid[i][k]; 
+        std::cout << "\n";
+    }
+}
+
+void opponentTurn() 
+{
+    std::cout << "-----------------" << "\n";
+    std::cout << "Oppenent's Turn" << "\n";
+    int col = takeColInput();
+    int lvl = dropCoin(col);
+    while (lvl == 0) {
+        std::cout << "Column is full, pick another" << "\n";
+        col = takeColInput();
+        lvl = dropCoin(col);
+    }
+    grid[lvl-1][col-1] = OPPONENT;
+}
+
+int takeColInput() {
+    int col;
+    while (true) {
+        std::cout << "What column? (1-7)" << std::endl;
+        std::cin >> col;
+        // thanks chat
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // discard bad input
+            std::cout << "Invalid input.";
+            continue;
+        }
+        if (col < 1 || col > 7) {
+            std::cout << "Invalid input.";
+            continue;
+        }
+        break;
+    }
+    return col;
+}
+
+int dropCoin(int column) // manipulates human values but uses computer values when parsing
+{
+    int lvl = 6;
+    while (lvl > 0) {
+        if (grid[lvl-1][column-1] == EMPTY) {
+            return lvl;
+        } else {
+            lvl--;
+        }
+    }
+}
+
